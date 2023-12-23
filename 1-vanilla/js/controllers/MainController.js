6b17fc0c8dd653e4987b9f3a20df9db8a77d220a
Jung JaeWoong <jjw_0609@naver.com>
2023-12-23 23:55:59 +0900

    최근 검색어 구현1, 2

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,9 +2,11 @@ import FormView from "../views/FormView.js";
 import ResultView from "../views/ResultView.js";
 import TabView from '../views/TabView.js';
 import KeywordView from '../views/KeywordView.js';
+import HistoryView from '../views/HistoryView.js';
 
 import SearchModel from "../models/SearchModel.js";
 import KeywordModel from "../models/KeywordModel.js";
+import HistoryModel from "../models/HistoryModel.js";
 
 const tag = '[MainController]';
 
@@ -20,9 +22,12 @@ export default {
         KeywordView.setup(document.querySelector('#search-keyword'))
             .on('@click', e => this.onClickKeyword(e.detail.keyword));
 
+        HistoryView.setup(document.querySelector('#search-history'))
+            .on('@click', e => this.onClickHistory(e.detail.keyword));
+
         ResultView.setup(document.querySelector('#search-result'))
 
-        this.selectedTab = '추천 검색어';
+        this.selectedTab = '최근 검색어';
         this.renderView();
     },
 
@@ -33,7 +38,7 @@ export default {
         if(this.selectedTab === '추천 검색어') {
             this.fetchSearchKeyword();
         } else {
-            debugger;
+            this.fetchSearchHistory();
         }
 
         ResultView.hide();
@@ -43,6 +48,12 @@ export default {
         KeywordModel.list().then(data => {
             KeywordView.render(data);
         });
+    },
+
+    fetchSearchHistory() {
+        HistoryModel.list().then(data => {
+            HistoryView.render(data);
+        })
     },
 
     onSubmit(input) {
@@ -73,6 +84,10 @@ export default {
     },
 
     onClickKeyword(keyword) {
+        this.search(keyword);
+    },
+
+    onClickHistory(keyword) {
         this.search(keyword);
     }
 }
---
+++ 1-vanilla/js/views/HistoryView.js
@@ -0,0 +1,7 @@
+import KeywordView from './KeywordView.js';
+
+const tag = '[HistoryView]';
+
+const HistoryView = Object.create(KeywordView);
+
+export default HistoryView;
