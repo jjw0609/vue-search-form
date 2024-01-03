db8d09852d38082f9b622ef2978bbb3ba5dbca5c
Jung JaeWoong <jjw_0609@naver.com>
2024-01-03 21:24:51 +0900

    검색폼 구현

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -5,7 +5,17 @@ new Vue({
     },
     methods: {
         onSubmit(e) {
+            debugger;
+        },
 
-        }
+        onKeyup() {
+          if(!this.query.length) this.onReset();
+        },
+
+        onReset() {
+            this.query = '';
+            // todo 검색결과를 숨기는 ...
+            debugger;
+        },
     }
 })
