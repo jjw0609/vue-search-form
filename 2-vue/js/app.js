97697a0e2fa61ce169148c13d5ffc9df25d15480
Jung JaeWoong <jjw_0609@naver.com>
2024-01-07 21:07:36 +0900

    검색결과

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,18 +1,33 @@
+import SearchModel from "./models/SearchModel.js";
+
 new Vue({
     el: '#app',
     data: {
-        query: 'abc'
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
-            debugger;
+            this.search();
         },
 
-        onKeyup() {
-          if(!this.query.length) this.onReset();
+        onKeyup(e) {
+          if(!this.query.length) this.resetForm();
         },
 
-        onReset() {
+        onReset(e) {
+            this.resetForm();
+        },
+
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true;
+                this.searchResult = data;
+            })
+        },
+
+        resetForm(e) {
             this.query = '';
             // todo 검색결과를 숨기는 ...
             debugger;
