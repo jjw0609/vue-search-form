1f0e3dba84cc29ac8856ddf286f826ddd7aefde1
Jung JaeWoong <jjw_0609@naver.com>
2024-02-19 22:36:16 +0900

    단일 컴포넌트3

--- 4-single-file-component/src/App.vue
+++ 4-single-file-component/src/App.vue
@@ -3,18 +3,77 @@
     <header>
       <h2 class="container">검색</h2>
     </header>
+    <div class="container">
+      <search-form v-bind:value="query" v-on:@submit="onSubmit" v-on:@reset="onReset"></search-form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HistoryModel from "../../3-component/js/models/HistoryModel.js";
+import KeywordModel from "../../3-component/js/models/KeywordModel.js";
+import SearchModel from "../../3-component/js/models/SearchModel.js";
+
+import FormComponent from "./components/FormComponent.vue";
 
 export default {
   name: 'App',
   data() {
     return {
-
+      query: '',
+      submitted: false,
+      tabs: ['추천 검색어', '최근 검색어'],
+      selectedTab: '',
+      keywords: [],
+      history: [],
+      searchResult: []
     };
+  },
+  components: {
+    'search-form': FormComponent
+  },
+  methods: {
+    onSubmit(query) {
+      this.query = query
+      this.search()
+    },
+    onReset(e) {
+      this.resetForm()
+    },
+    onClickTab(tab) {
+      this.selectedTab = tab
+    },
+    onClickKeyword(keyword) {
+      this.query = keyword;
+      this.search()
+    },
+    onClickRemoveHistory(keyword) {
+      HistoryModel.remove(keyword)
+      this.fetchHistory()
+    },
+    fetchKeyword() {
+      KeywordModel.list().then(data => {
+        this.keywords = data
+      })
+    },
+    fetchHistory() {
+      HistoryModel.list().then(data => {
+        this.history = data
+      })
+    },
+    search() {
+      SearchModel.list().then(data => {
+        this.submitted = true
+        this.searchResult = data
+      })
+      HistoryModel.add(this.query)
+      this.fetchHistory()
+    },
+    resetForm() {
+      this.query = ''
+      this.submitted = false
+      this.searchResult = []
+    }
   }
 }
 </script>
